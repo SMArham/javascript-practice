c0d6ba12ac571a97133f1722d02454fff8e030f6
Syed Muhammad Arham <syedarhamreal@gmail.com>
2024-04-04 15:15:29 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// for(var i = 1; i > 0; i++) {
+// console.log("hello");
+// }
+
+// // for(var i = 1; i <= 10; i++) {
+// //     document.write("2" +" x " +i +"=" +2*i + "<br>")
+// // }
+  
+// // for(var i = 1; i <= 10; i++){
+// //     document.write("2" +"x" +i +"=" +2*i +"<br>")
+// // }
+  
+
+// // var cities= ["europe" , "usa" , "uk"];
+// // for(var i =0; i < cities.length; i++){
+// //     console.log(cities[i]);
+// // }
+
+
+// var cities= ["europe" , "usa" , "uk", "england"];
+
+// for (var i = 0; i < cities.length; i++ ) {
+//     if (cities [i] ==="uk"){
+//         alert(cities[i] +" is no 1")
+//          break
+//     }
+// }
+
+ 
+
+for(var i = 1; i < 5; i++){
+ for(var j= 0; j < 3; j++){
+document.write("good" + "`x<br>")}
+}
+
+
+
+
